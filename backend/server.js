f3718470e5368d24cf18496025da5ca2c6bf7baa
atharva-nile <atharva@intandem.co.in>
2025-12-07 23:36:23 +0530

    Production changes.

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(
   cors({
     origin: "*",
+    methods: "GET,POST,PUT,DELETE,PATCH",
+    allowedHeaders: "Content-Type, Authorization",
   })
 );
 app.use(express.json());
